168ef3535292377c18c67c3047d2226e158c207d
Peter M. Gruszka <gruszka@physik.uni-frankfurt.de>
2017-10-18 12:24:28 +0200

    fixed some parts in relais.py

--- USBDII-x64/relais.py
+++ USBDII-x64/relais.py
@@ -23,13 +23,11 @@ else:
 
 
 class RelaisCardSimulation:
-    def __init__(self, card_type, path=''):
+    def __init__(self, card_type, path=' '):
         self.channels = [0] * 4
-
 
     def check_connection(self, card_number):
         return True
-
 
     def connect(self, card_number):
         return True
@@ -55,11 +53,11 @@ class RelaisCard:
             self.__card_type = c_uint(CardType[card_type])
 
         self.__device_path = c_char_p(path)
-        self.__handle = 0
+        self.__handle = -1
 
 
     def set_device_path(self, newpath):
-        self.__device_path = newpath
+        self.__device_path = c_char_p(newpath)
 
     def check_connection(self, card_number):
         result = self.connect(card_number)
@@ -68,10 +66,11 @@ class RelaisCard:
 
 
     def connect(self, card_number):
-        self.__handle = 0
+        self.__handle = -1
 
         if os.name == "posix":
-            self.__handle = usbdii.dcihid_open(self.__device_path, self.__card_type, card_number)
+            self.__handle = usbdii.dcihid_open(self.__device_path,
+                                               self.__card_type, card_number)
         elif os.name == "nt":
             self.__handle = usbdii.hid_OpenDevice(self.__card_type, card_number)
 
@@ -81,16 +80,16 @@ class RelaisCard:
         return True
 
     def disconnect(self):
-        if self.__handle != 0:
+        if self.__handle != -1:
             handle_c = c_uint(self.__handle)
             if os.name == 'posix':
                 usbdii.dcihid_close(handle_c)
             elif os.name == 'nt':
                 usbdii.hid_CloseDevice(handle_c)
-            self.__handle = 0
+            self.__handle = -1
 
     def set_digital_byte(self, channel, byte):
-        if(self.__handle == 0):
+        if(self.__handle == -1):
             raise Exception("Device not connected")
 
         channel_c = c_uint(channel)
@@ -99,20 +98,23 @@ class RelaisCard:
 
         res = -1
 
-	if os.name == 'nt':
+        if os.name == 'nt':
             res = usbdii.hid_SetDigitalByte(handle_c, channel_c, byte_c)
         elif os.name == 'posix':
             res = usbdii.dcihid_write(handle_c, channel_c, byte_c)
- 
+
         if res == -1:
             raise Exception("Error: sending byte unsuccessful! Maybe USB connection was interrupted. Please connect device and restart program.")
 
     def get_digital_byte(self, channel):
+        if(self.__handle == -1):
+            raise Exception("Device not connected")
+
         byte = c_ubyte(0)
         channel_c = c_uint(channel)
         handle_c = c_uint(self.__handle)
 
-	if os.name == 'nt':
+        if os.name == 'nt':
             usbdii.hid_GetDigitalByte(handle_c, channel_c, byref(byte))
         elif os.name ==  'posix':
             usbdii.dcihid_read(handle_c, channel_c, byref(byte))

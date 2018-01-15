e9fdd80d4e8d4d08eff685d97571276ca6ca46c9
NanoPeter <gruszka@physik.uni-frankfurt.de>
2018-01-15 17:11:35 +0100

    finished flowcontroller

--- devices/alicat/flowcontroller.py
+++ devices/alicat/flowcontroller.py
@@ -9,7 +9,7 @@ class FlowControllerResult(object):
             self.__init(split)
 
     def __init(self, split):
-        self.pressure = float(split[1])
+        self.pressure = 70.0 * float(split[1]) #umrechnung von psia in mbar
         self.temperature = float(split[2])
         self.volflow = float(split[3])
         self.massflow = float(split[4])
@@ -22,6 +22,8 @@ class FlowControllerResult(object):
         self.massflow = 0
         self.setpoint = 0
 
+    def __repr__(self):
+        return 'Pressure: {0}\nTemperature: {1}\nVolume Flow: {2}\nMass Flow: {3}\nSet Point: {4}\n'.format(self.pressure, self.temperature, self.volflow, self.massflow, self.setpoint)
 
 class FlowController(object):
     def __init__(self, connection, unit_id='A'):
@@ -43,24 +45,35 @@ class FlowController(object):
         return FlowControllerResult(raw_message)
 
     def set(self, value):
-        parameter = self.__´calculate_parameter(value)
+        parameter = self.__calculate_parameter(value)
 
         self.connection.write('{0}{1}\r'.format(self.unit_id, parameter))
         raw_message = self.connection.readline()
 
         return FlowControllerResult(raw_message)
 
+    def off(self):
+        return self.set(0.0)
+
     def __calculate_parameter(self, value):
         value = min(100, max(value, 0)) #maximal 100 sccm and minimum 0 sccm
         return  int((value * 64000) / 100)
 
 if __name__ == '__main__':
+    
+    import sys
+
+    flow = 0
+    if len(sys.argv) == 2:
+        flow = float(sys.argv[1])
+
     import serial
     S = serial.Serial('/dev/ttyUSB3')
 
     fc = FlowController(S)
 
     print(fc.poll())
-    print(fc.set(10.0))
+    print(fc.set(flow))
+    fc.off()
 
 
